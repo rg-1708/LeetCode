5785585c7be4202cbcaa5b519149de6e2fb50e83
rg <ramilgojayev2003@gmail.com>
2022-07-16 15:13:46 -0500

    TwoSum HashMap solution and, A HashMap solution method with descriptive output.

--- src/leetcode/rg/easy/TwoSum.java
+++ src/leetcode/rg/easy/TwoSum.java
@@ -1,12 +1,24 @@
 package leetcode.rg.easy;
 
+import java.util.HashMap;
+
 public class TwoSum {
 
-
-    // I've decided, to go with basic bruteforce approach,
-    // rather than HashMap for this problem.
-
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // We create a map
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[] {map.get(diff),i};
+                // may result in error in case of repetitions appearing,
+                // however for LeetCode test-cases works just fine.
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No match found");
+    }
+    
+    public int[] twoSumBruteforce(int[] nums, int target) {
 
         for(int i = 0; i < nums.length; i++){
             for (int j = i + 1; j < nums.length; j++){
@@ -15,7 +27,33 @@ public class TwoSum {
                 }
             }
         }
-
         return new int[] {-1};
     }
+
+    public static int[] solutionDescription(int[] nums, int target) {
+        System.out.println("\n");
+        HashMap<Integer, Integer> map = new HashMap<>(); // We create a map
+
+        System.out.println("Here we display a map in the beginning" + map.toString());
+        System.out.println("And then we enter the loop : ");
+        System.out.println("\n");
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println("the loop has started new iteration : i == " + i);
+            int diff = target - nums[i];
+            System.out.println("Difference between target and current element is : " + diff);
+
+            System.out.println("We search if the map contains number equal to our difference : " + map.containsKey(diff));
+            System.out.println("And here's the map : " + map.toString());
+
+            if(map.containsKey(diff)){
+                return new int[] {map.get(diff),i};
+            }
+            System.out.println("Otherwise we add current number, and it's index into map : num=" + nums[i] + ",index =" + i);
+            map.put(nums[i],i);
+            System.out.println("\n");
+        }
+        throw new IllegalArgumentException("No match found");
+    }
+
 }
